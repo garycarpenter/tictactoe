bedd4172729615662e0834a52090e955dbb99307
garycarpenter <113570311+garycarpenter@users.noreply.github.com>
2023-03-27 11:30:34 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tictactoe
+
+Implement a console game of tic tac toe played between two humans in ruby
---
+++ tictactoe_game.rb
@@ -0,0 +1,16 @@
+#tictactoe_game.rb
+
+#TODO implement game board
+#TODO implement player class
+
+class GameBoard
+
+end
+
+class Player
+
+end
+
+
+
+
